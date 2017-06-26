43479594187b3c12c03ce4d3fe8f2e7bda5d5f7d
Nick Duskey <me@nickduskey.com>
2017-06-26 14:15:40 -0400

    Updated SCSS load order

--- src/js/sections/Intro.js
+++ src/js/sections/Intro.js
@@ -5,24 +5,23 @@ import StartButton from '../components/StartButton';
 import Headline from 'grommet/components/Headline';
 import Box from 'grommet/components/Box';
 import Footer from 'grommet/components/Footer';
-import UserPolice from 'grommet/components/icons/base/UserPolice';
+import User from 'grommet/components/icons/base/User';
 import UserManager from 'grommet/components/icons/base/UserManager';
-import UserWorker from 'grommet/components/icons/base/UserWorker';
+import UserFemale from 'grommet/components/icons/base/UserFemale';
 
 export default function Intro () {
   return (
     <InfographicSection className="infographic__section infographic__section--start"
       colorIndex="light-1">
       <div className="infographic__section-header">
-        <Headline size="large" strong={true}>Reshaping the Economy</Headline>
+        <Headline size="large" strong={true}>Nick Duskey, Product Manager</Headline>
       </div>
-      <Headline size="small">As Millennials move into prime spending years, companies
-        must reinvent how they do business.</Headline>
+      <Headline size="small">Bringing new products to life and empowering teams to make the best products possible.</Headline>
       <Box direction="row" full="horizontal" justify="center" align="center"
         pad={{vertical: "medium"}} responsive={false}>
         <UserManager size="xlarge" colorIndex="neutral-2" />
-        <UserPolice size="xlarge" colorIndex="neutral-1" />
-        <UserWorker size="xlarge" colorIndex="neutral-2" />
+        <UserFemale size="xlarge" colorIndex="neutral-1" />
+        <User size="xlarge" colorIndex="neutral-2" />
       </Box>
       <Footer justify="center" align="center">
         <StartButton text="scroll down to continue"/>
